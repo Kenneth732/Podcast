66d96057ee0756b301c879bac19850863c4d584e
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-11-02 15:23:59 +0300

    hero

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className="home_container">
+        <div className="left_home">
+          <h1>Starting a podcast has never been easier</h1>
+
+          <ul className="do">
+            <li className="list">
+              <i class="fas fa-check-circle"></i>
+              <p>One click integrations, that work for you.</p>
+            </li>
+
+            <li className="list">
+              <i class="fas fa-check-circle"></i>
+              <p>One click integrations, that work for you.</p>
+            </li>
+
+            <li className="list">
+              <i class="fas fa-check-circle"></i>
+              <p>One click integrations, that work for you.</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="right_home">
+          <img src="https://wavvy-next-obfuscated-preview.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhome-hero.398a4a99.jpg&w=640&q=75" className='profile' alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
